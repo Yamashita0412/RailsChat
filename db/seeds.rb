1bab964f4e8c7d5f0b09f97fd8450e7138d62c24
Kyonosuke Yamashita <bp16103@shibaura-it.ac.jp>
2018-06-15 23:47:22 +0900

    change seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create!(name:  "Example User2",
              password_confirmation: "foobar",
              admin: true)
 
-99.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
